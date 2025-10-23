98c9d64ec19a3aa9bdcdb5724ab9d7f7cca42a2c
susmitha.chinnari <200940547+susmithachinnari@users.noreply.github.com>
2025-10-23 15:55:47 +0100

    Skeleton draft created for testing DTR-813

--- src/test/scala/uk/gov/hmrc/ui/specs/InitialSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/InitialSpec.scala
@@ -21,7 +21,7 @@ import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, GivenWhenThen}
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.verbs.ShouldVerb
 import uk.gov.hmrc.selenium.webdriver.{Browser, ScreenshotOnFailure}
-import uk.gov.hmrc.ui.pages.{AuthWizard, IndividualOrCompanyPage, InitialPage, IsAnIndividualPage}
+import uk.gov.hmrc.ui.pages.{AuthWizard, CYAPage, IndividualOrCompanyPage, InitialPage, IsAnIndividualPage}
 import uk.gov.hmrc.ui.util.Users.LoginTypes.HASDIRECT
 import uk.gov.hmrc.ui.util.Users.UserTypes.Organisation
 
@@ -36,38 +36,53 @@ class InitialSpec
     with ScreenshotOnFailure {
 
   Feature("SDLT Filing frontend Task List Homepage") {
-    Scenario("Hit the TaskList with no return id") {
+  /*  Scenario("Update purchaser type from CYA page") {
       Given("I enter login using the Authority Wizard page")
       AuthWizard.login(HASDIRECT, Organisation)
-      Then("I should be on the initial page")
-      InitialPage.verifyPageTitle(InitialPage.pageTitle)
-      InitialPage.verifyTaskRowStatus("status-prelimQuestionDetailRow", "Not Started")
-      Then("User clicks Prelim Questions Task")
-      InitialPage.clickLinkById("task-list-link-prelim-questions")
-      Then("User should be on the Individual or Company Name page")
+      And("user navigates to check your answers page")
+      CYAPage.navigateToPage(CYAPage.pageUrl)
+      Then("user should be on check your answers page")
+      CYAPage.verifyPageTitle(CYAPage.pageTitle)
+      When("user clicks on change for purchaser type")
+      CYAPage.clickLinkById(CYAPage.changePurchaserType)
+      Then("user should be on individual or company page")
       IndividualOrCompanyPage.verifyPageTitle(IndividualOrCompanyPage.pageTitle)
-      Then("User input their name or company name and submits")
-      IndividualOrCompanyPage.input(By.id("purchaserSurnameOrCompanyName"), "Test Name")
-      IndividualOrCompanyPage.clickSubmitButton()
-      Then("User should be on Is the User and Individual Page")
+    }
+    Scenario("Update purchaser's name form CYA page") {
+      Given("I enter login using the Authority Wizard page")
+      AuthWizard.login(HASDIRECT, Organisation)
+      And("user navigates to check your answers page")
+      CYAPage.navigateToPage(CYAPage.pageUrl)
+      Then("user should be on check your answers page")
+      CYAPage.verifyPageTitle(CYAPage.pageTitle)
+      When("user clicks on change for purchaser name")
+      CYAPage.clickLinkById(CYAPage.changePurchaserName)
+      Then("user should be on purchaser's name page")
       IsAnIndividualPage.verifyPageTitle(IsAnIndividualPage.pageTitle)
     }
-
-    Scenario("Hit the TaskList with a return id") {
+    Scenario("Update property address form CYA page") {
       Given("I enter login using the Authority Wizard page")
-      AuthWizard.login(HASDIRECT, Organisation, Some("123456"))
-      Then("I should be on the initial page")
-      InitialPage.verifyPageTitle(InitialPage.pageTitle)
-      InitialPage.verifyTaskRowStatus("status-prelimQuestionDetailRow", "Complete")
-      Then("User clicks Prelim Questions Task")
-      InitialPage.clickLinkById("task-list-link-prelim-questions")
-      Then("User should be on the Individual or Company Name page")
-      IndividualOrCompanyPage.verifyPageTitle(IndividualOrCompanyPage.pageTitle)
-      Then("User input their name or company name and submits")
-      IndividualOrCompanyPage.input(By.id("purchaserSurnameOrCompanyName"), "Test Name")
-      IndividualOrCompanyPage.clickSubmitButton()
-      Then("User should be on Is the User and Individual Page")
-      IsAnIndividualPage.verifyPageTitle(IsAnIndividualPage.pageTitle)
+      AuthWizard.login(HASDIRECT, Organisation)
+      And("user navigates to check your answers page")
+      CYAPage.navigateToPage(CYAPage.pageUrl)
+      Then("user should be on check your answers page")
+      CYAPage.verifyPageTitle(CYAPage.pageTitle)
+      When("user clicks on change for property address")
+      CYAPage.clickLinkById(CYAPage.changePropertyAddress)
+      Then("user should be on property address page")
+      PropertyAddressPage.verifyPageTitle(PropertyAddressPage.pageTitle)
+    }
+    Scenario("Update transaction type  form CYA page") {
+      Given("I enter login using the Authority Wizard page")
+      AuthWizard.login(HASDIRECT, Organisation)
+      And("user navigates to check your answers page")
+      CYAPage.navigateToPage(CYAPage.pageUrl)
+      Then("user should be on check your answers page")
+      CYAPage.verifyPageTitle(CYAPage.pageTitle)
+      When("user clicks on change for transaction type")
+      CYAPage.clickLinkById(CYAPage.changeTransactionType)
+      Then("user should be on transaction type page")
+      TransactionPage.verifyPageTitle(TransactionPage.pageTitle)*/
     }
   }
 }
