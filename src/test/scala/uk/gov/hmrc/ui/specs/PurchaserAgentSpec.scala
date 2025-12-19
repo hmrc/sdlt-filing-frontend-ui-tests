bc2a095c0d91c151b5ac1c83c02a209fc8ab3a5f
susmitha.chinnari <200940547+susmithachinnari@users.noreply.github.com>
2025-12-19 10:43:35 +0000

    Added UI tests for DTR-1802

---
+++ src/test/scala/uk/gov/hmrc/ui/specs/PurchaserAgentSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.specs
+
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.verbs.ShouldVerb
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, GivenWhenThen}
+import uk.gov.hmrc.selenium.webdriver.{Browser, ScreenshotOnFailure}
+import uk.gov.hmrc.ui.pages.*
+import uk.gov.hmrc.ui.pages.PurchaserAgent.PurchaserAgentBeforeYouStartPage
+import uk.gov.hmrc.ui.util.Users.LoginTypes.HASDIRECT
+import uk.gov.hmrc.ui.util.Users.UserTypes.Organisation
+
+class PurchaserAgentSpec
+    extends AnyFeatureSpec
+    with BaseSpec
+    with GivenWhenThen
+    with ShouldVerb
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach
+    with Browser
+    with ScreenshotOnFailure {
+
+  Feature("SDLT Filing Frontend Purchaser Agent Journey") {
+    Scenario("Complete the Purchaser Agent Journey") {
+      Given("the user logs in through the Authority Wizard page")
+      AuthWizard.login(HASDIRECT, Organisation)
+      Then("the user is navigated to the Return Task List page")
+      ReturnTaskListPage.navigateToPage(ReturnTaskListPage.pageUrl)
+      When("the user clicks on the 'Purchaser Agent Questions' link")
+      PurchaserAgentBeforeYouStartPage.clickLinkById("task-list-link-purchaser-agent-questions")
+      Then("the user should be navigated to the Before You Start page")
+      PurchaserAgentBeforeYouStartPage.verifyPageTitle(PurchaserAgentBeforeYouStartPage.pageTitle)
+      And("user selects 'Yes' radio button")
+      PurchaserAgentBeforeYouStartPage.radioButton(PurchaserAgentBeforeYouStartPage.yes)
+      And("clicks the continue button")
+      PurchaserAgentBeforeYouStartPage.saveAndContinue()
+    }
+  }
+}
