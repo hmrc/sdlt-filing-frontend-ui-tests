20d32f33a3d1d5bbff2760e269a72c20a56dc868
tatheerfatima-lab <236847356+tatheerfatima-lab@users.noreply.github.com>
2026-02-25 10:05:50 +0000

    Add Area of the Land Page tests

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/Land/AddAreaOfTheLand.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages.Land
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.ui.pages.BasePage
+
+object AddAreaOfTheLand extends BasePage {
+
+  override def pageUrl: String   = "about-the-land/add-area-of-land"
+  override def pageTitle: String = "Do you know the area of the land? – About the land - Stamp Taxes Online - GOV.UK"
+
+  val yes: String = "#value_0"
+
+  val no: String = "#value_1"
+}
