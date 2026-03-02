0322c685c39a4c413301298b9c9e1ac7a8ab9c7b
tatheerfatima-lab <236847356+tatheerfatima-lab@users.noreply.github.com>
2026-03-02 21:33:36 +0000

    tag added

--- run_tests_local.sh
+++ run_tests_local.sh
@@ -2,4 +2,4 @@
 
 BROWSER=$1
 ENVIRONMENT=$2
-sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean compile -Dbrowser="${BROWSER:=chrome}" -Daccessibility.timeout=10000.millis -Denvironment="${ENVIRONMENT:=local}" -Dbrowser.usePreviousVersion=true -Dbrowser.option.headless=true "testOnly uk.gov.hmrc.ui.specs*" testReport
+sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean compile -Dbrowser="${BROWSER:=chrome}" -Daccessibility.timeout=10000.millis -Denvironment="${ENVIRONMENT:=local}" -Dbrowser.usePreviousVersion=true -Dbrowser.option.headless=true "testOnly uk.gov.hmrc.ui.specs* -- -n uk.gov.hmrc.ui.tags.LandJourney" testReport
