af3d644c4a3ab6d0e912dc74f2ea4044a89ce338
samirsshahid <206914350+samirsshahid@users.noreply.github.com>
2026-03-23 15:36:58 +0000

    Improved readability for cya navigations

--- src/test/scala/uk/gov/hmrc/ui/specs/VendorAgentQuestionsSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/VendorAgentQuestionsSpec.scala
@@ -227,18 +227,6 @@ class VendorAgentQuestionsSpec
       Then("the Vendor Agent contact details are updated on the Check Your Answers page")
       VendorAgentCheckYourAnswers.verifyPageTitle(VendorAgentCheckYourAnswers.pageTitle)
 
-      When("the user chooses to enter contact details")
-      VendorAgentCheckYourAnswers.clickVendorAgentContactDetailsChange()
-      VendorAgentEnterContactDetails.verifyPageTitle(VendorAgentEnterContactDetails.pageTitle)
-      VendorAgentEnterContactDetails.input(
-        By.id(VendorAgentEnterContactDetails.phoneNumber),
-        VendorAgentEnterContactDetails.phoneNumberInput
-      )
-      VendorAgentEnterContactDetails.saveAndContinue()
-
-      Then("the Vendor Agent contact details are updated on the Check Your Answers page")
-      VendorAgentCheckYourAnswers.verifyPageTitle(VendorAgentCheckYourAnswers.pageTitle)
-
       When("the user chooses to add a reference number")
       VendorAgentCheckYourAnswers.clickDoYouWantToAddReferenceChange()
       AddVendorAgentReferenceNumber.verifyPageTitle(AddVendorAgentReferenceNumber.pageTitle)
