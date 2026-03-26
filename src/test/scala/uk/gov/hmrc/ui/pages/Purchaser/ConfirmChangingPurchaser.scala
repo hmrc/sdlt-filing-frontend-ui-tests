607a19f2f6fbe3215b378bc91c81d4661e2506ca
samirsshahid <206914350+samirsshahid@users.noreply.github.com>
2026-03-26 09:34:22 +0000

    Added confirm changing purchaser page & ui tests

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/Purchaser/ConfirmChangingPurchaser.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages.Purchaser
+
+import uk.gov.hmrc.ui.pages.BasePage
+
+object ConfirmChangingPurchaser extends BasePage {
+
+  override def pageUrl: String = "about-the-purchaser/confirm-changing-purchaser-1"
+
+  override def pageTitle: String = "Are you sure you want to change purchaser 1? - About the purchaser - Stamp Taxes Online - GOV.UK"
+
+  def yes: String = "#value"
+
+  def no: String = "#value-2"
+
+}
