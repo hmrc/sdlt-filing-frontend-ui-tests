e5fccff9787b8304bab87b515a975f40db3de7d0
tatheerfatima-lab <236847356+tatheerfatima-lab@users.noreply.github.com>
2026-04-09 16:42:17 +0100

    Is Vat included in total consideration tests added

--- src/test/scala/uk/gov/hmrc/ui/specs/TransactionQuestionsSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/TransactionQuestionsSpec.scala
@@ -180,52 +180,60 @@ class TransactionQuestionsSpec
 
       /* user enters date of contract or conclusion of missives and continues
       user navigated to total consideration of transaction page
-        user enters total consideration of transaction amount and continues
-      user is navigated to Is VAT included in the total consideration ? page
-      user selects yes radio button and continues
-      user is navigated ro enter VAT amount page
-        user enters VAT amount and continues
-        user is navigated to forms of consideration page
-        user selects check boxes and continues
-        user navigates to Linked transactions page
-        user selects yes radio button and continues
-      user navigates to total consideration of all linked transactions page
-        user enters the amount and continues
-        user is navigated to claiming relief page
-      user selects yes radio button and continues
-      user navigates to reasonfor claiming relief page
-            user selects part exchange radio button and continues
-            user is navigated to DO you know the company CIS number page
-        user selects yes radio button and continues
-      user enters CIS number and continues
-      user is navigated to Partial relief page
-      user selects yes radio button and continues
-      user is navigated to claiming partial relief page
-        user enters the claimed amount and continues
-      user navigated to considerations effected by uncertain future events page
-        user selects yes radio button and continues
-      user is navigated to deferring payment page
-      user selects yes radio button and continues
-      user is navigated to the use of land or property page(Only if property type is selected as non - residential or mixed )
-      user selects checkboxes and continues
-      user is navigated to Sale of business page
-        user selects yes radio button and continues
-      user is navigated to what is included in sale of business page
-        user selects checkboxes and continues(select others option checkbox)
-      user is navigated to total consideration of all assets included in sale of business page
-      user enters the amount and continues
-        user is navigated to the CAP1 or NSBC page
-      user selects yes radio button and continues
-      user is navigated to restrictions convenants and conditions page
-      user selects yes radio button and continues
-      user is navigated to enter details of restrictions convenants and conditions page
-        user enters the details and continues
-        user is navigated to exchange or part exchange of land or property page
-      user selects yes radio button and continues
-      user is navigated to address flow and fills the details
-        user navigates to Exercising an option page
-      user selects yes radio button and continues
-      user is navigated to CYA page and verifies the answers and submits*/
+        user enters total consideration of transaction amount and continues*/
+      Then("the user is navigated to Is VAT included in the total consideration page")
+      IsVatIncludedInTotalConsideration.navigateToPage(
+        "http://localhost:10910/stamp-duty-land-tax-filing/about-the-transaction/is-VAT-included"
+      )
+      IsVatIncludedInTotalConsideration.verifyPageTitle(IsVatIncludedInTotalConsideration.pageTitle)
+
+      When("the user selects yes radio button and continues")
+      IsVatIncludedInTotalConsideration.radioButton(IsVatIncludedInTotalConsideration.yes)
+      IsVatIncludedInTotalConsideration.saveAndContinue()
+
+      /* user is navigated ro enter VAT amount page
+         user enters VAT amount and continues
+         user is navigated to forms of consideration page
+         user selects check boxes and continues
+         user navigates to Linked transactions page
+         user selects yes radio button and continues
+       user navigates to total consideration of all linked transactions page
+         user enters the amount and continues
+         user is navigated to claiming relief page
+       user selects yes radio button and continues
+       user navigates to reasonfor claiming relief page
+             user selects part exchange radio button and continues
+             user is navigated to DO you know the company CIS number page
+         user selects yes radio button and continues
+       user enters CIS number and continues
+       user is navigated to Partial relief page
+       user selects yes radio button and continues
+       user is navigated to claiming partial relief page
+         user enters the claimed amount and continues
+       user navigated to considerations effected by uncertain future events page
+         user selects yes radio button and continues
+       user is navigated to deferring payment page
+       user selects yes radio button and continues
+       user is navigated to the use of land or property page(Only if property type is selected as non - residential or mixed )
+       user selects checkboxes and continues
+       user is navigated to Sale of business page
+         user selects yes radio button and continues
+       user is navigated to what is included in sale of business page
+         user selects checkboxes and continues(select others option checkbox)
+       user is navigated to total consideration of all assets included in sale of business page
+       user enters the amount and continues
+         user is navigated to the CAP1 or NSBC page
+       user selects yes radio button and continues
+       user is navigated to restrictions convenants and conditions page
+       user selects yes radio button and continues
+       user is navigated to enter details of restrictions convenants and conditions page
+         user enters the details and continues
+         user is navigated to exchange or part exchange of land or property page
+       user selects yes radio button and continues
+       user is navigated to address flow and fills the details
+         user navigates to Exercising an option page
+       user selects yes radio button and continues
+       user is navigated to CYA page and verifies the answers and submits*/
     }
   }
 }
