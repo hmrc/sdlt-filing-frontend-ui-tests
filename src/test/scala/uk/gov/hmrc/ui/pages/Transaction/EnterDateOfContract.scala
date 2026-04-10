a6ee55cca18b1d3c71404204028769f1585cd4e6
samirsshahid <206914350+samirsshahid@users.noreply.github.com>
2026-04-10 14:18:25 +0100

    Added Enter date of contract page and UI tests

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/Transaction/EnterDateOfContract.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages.Transaction
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.ui.pages.BasePage
+
+object EnterDateOfContract extends BasePage {
+
+  override def pageUrl: String = "about-the-transaction/enter-date-of-contract"
+
+  override def pageTitle: String =
+    "What is the date of contract or conclusion of missives? - About the transaction - Stamp Taxes Online - GOV.UK"
+
+  val day: String = "value.day"
+
+  val dayInput: String = "30"
+
+  val month: String = "value.month"
+
+  val monthInput: String = "03"
+
+  val year: String = "value.year"
+
+  val yearInput: String = "2004"
+
+  def enterDateOfContract(): Unit = inputDateMonthAndYear(
+    By.id(day),
+    By.id(month),
+    By.id(year),
+    dayInput,
+    monthInput,
+    yearInput
+  )
+
+}
