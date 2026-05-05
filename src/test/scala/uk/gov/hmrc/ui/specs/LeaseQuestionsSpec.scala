8bb8412a3e339540fde0688473d9ee85902dd33b
susmitha.chinnari <200940547+susmithachinnari@users.noreply.github.com>
2026-05-05 11:50:47 +0100

    UI tests added for Lease Before you start page

---
+++ src/test/scala/uk/gov/hmrc/ui/specs/LeaseQuestionsSpec.scala
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.specs
+
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.verbs.ShouldVerb
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, GivenWhenThen}
+import uk.gov.hmrc.selenium.webdriver.{Browser, ScreenshotOnFailure}
+import uk.gov.hmrc.ui.pages.*
+import uk.gov.hmrc.ui.pages.Lease.*
+import uk.gov.hmrc.ui.tags.*
+import uk.gov.hmrc.ui.util.Users.LoginTypes.HASDIRECT
+import uk.gov.hmrc.ui.util.Users.UserTypes.Organisation
+
+class LeaseQuestionsSpec
+    extends AnyFeatureSpec
+    with BaseSpec
+    with GivenWhenThen
+    with ShouldVerb
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach
+    with Browser
+    with ScreenshotOnFailure {
+
+  Feature("SDLT Filing Frontend Lease Questions") {
+
+    Scenario(
+      "Complete the Lease Questions journey when transaction type is selected as L-grant of lease",
+      LeaseJourney
+    ) {
+
+      Given("the user is logged in through the AuthWizard page")
+      AuthWizard.login(HASDIRECT, Organisation, returnId = Some("firstJson"))
+
+      When("the user starts the Lease questions")
+      ReturnTaskList.clickLinkById("task-list-link-lease-questions")
+      Then("Lease questions Before you start page is shown")
+      LeaseBeforeYouStart.verifyPageTitle(LeaseBeforeYouStart.pageTitle)
+      // Uncomment the below code when the navigation is ready
+      // LeaseBeforeYouStart.clickContinueButton()
+
+      /*
+      Type of Lease Page is shown
+        User selects the type of lease as  and continues
+        Lease start date page is shown
+        USer selects the Lease start date and continues
+        Lease end date page is shown
+        User selects the Lease end date and continues
+        Rent free periods page is shown
+        User selects the yes for rent-free periods and continues
+        Calculating the rent-free periods page is shown
+        User enters the rent-free periods months and continues
+        Annual starting rent page is shown
+        User enters the annual starting rent and continues
+        End of annual starting rent page is shown
+        User selects the end date of annual starting rent and continues
+        Later rent page is shown
+        User selects the yes for later rent and continues
+        £1000 threshold page is shown
+        User selects the yes for £1000 threshold and continues
+        Annual rent VAT page is shown
+        User selects the yes for annual rent VAT and continues
+        Total amount of VAT payable on annual rent is shown
+         User enters the total amount of VAT payable on annual rent and continues
+        Total premium payable page is shown
+        User selects the yes for total premium payable and continues
+        Calculating the total premium payable including VAT page is shown
+        User enters the total premium payable including VAT and continues
+        Net present value page is shown
+        User enters the net present value and continues
+        Check your answers page is shown
+         User checks the answers and continues
+         Overview page is shown
+
+
+       */
+    }
+
+    Scenario(
+      "Complete the Lease Questions journey when transaction type is not L-grant of lease",
+      LeaseJourney
+    ) {
+      Given("the user is logged in through the AuthWizard page")
+      AuthWizard.login(HASDIRECT, Organisation, returnId = Some("firstJson"))
+
+      When("the user starts the Lease questions")
+      ReturnTaskList.clickLinkById("task-list-link-lease-questions")
+      Then("Lease questions Before you start page is shown")
+      LeaseBeforeYouStart.verifyPageTitle(LeaseBeforeYouStart.pageTitle)
+      // Uncomment the below code when the navigation is ready
+      // LeaseBeforeYouStart.clickContinueButton()
+
+      /*
+ Type of Lease Page is shown
+ User selects the type of lease and continues
+ Lease start date page is shown
+ USer selects the Lease start date and continues
+ Lease end date page is shown
+ User selects the Lease end date and continues
+ Rent free periods page is shown
+ User selects the yes for rent - free periods and continues
+ Calculating the rent - free periods page is shown
+ User enters the rent - free periods months and continues
+ Annual starting rent page is shown
+ User enters the annual starting rent and continues
+       End of annual starting rent page is shown
+       User selects the end date of annual starting rent and continues
+       Later rent page is shown
+       User selects the yes for later rent and continues
+        User selects the yes for £1000 threshold and continues
+   Annual rent VAT page is shown
+   User selects the yes for annual rent VAT and continues
+   Total amount of VAT payable on annual rent is shown
+    User enters the total amount of VAT payable on annual rent and continues
+    Check your answers page is shown
+    User checks the answers and continues
+    Overview page is shown
+       */
+    }
+  }
+}
