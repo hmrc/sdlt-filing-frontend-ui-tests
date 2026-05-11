3e823a4d94d0c409874d2f4a02dacf0f54d14516
Jyothi Gadipudi <247127109+jyothigadipudi-tester@users.noreply.github.com>
2026-05-11 15:54:15 +0100

    rebase with main

--- src/test/scala/uk/gov/hmrc/ui/specs/TaxCalculationsQuestionsSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/TaxCalculationsQuestionsSpec.scala
@@ -195,6 +195,21 @@ class TaxCalculationsQuestionsSpec
       Then("the Freehold calculated Before you start page is displayed")
       TaxCalculationsBeforeYouStart.verifyPageTitle(TaxCalculationsBeforeYouStart.pageTitleLeaseholdNotCalculated)
 
+      /*Scenario 4
+    user is navigated to What is the tax due on total premium payable page
+    user enter the total premium payable and click save and continue button*/
+      Then("user is navigated to what is the tax due on the NPV page")
+      TaxDueOnNPV.navigateToPage(
+        "http://localhost:10910/stamp-duty-land-tax-filing/tax-calculation/leasehold-not-calculated/tax-due-on-NPV"
+      )
+      TaxDueOnNPV.verifyPageTitle(TaxDueOnNPV.pageTitle)
+      When("user enter the NPV value and click save and continue button")
+      TaxDueOnNPV.input(
+        By.id(TaxDueOnNPV.taxDueOnNPVAmountInput),
+        TaxDueOnNPV.taxDueOnNPVAmount
+      )
+      TaxDueOnNPV.saveAndContinue()
+
       // user enter the total amount you intend to pay with this return and click save and continue
       Then("the user is navigated to the total premium value page")
       TaxCalculationsTotalPremiumValueLeaseholdSelfassesed.navigateToPage(
