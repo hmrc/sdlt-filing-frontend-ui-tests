3c5d711be92c76ee9482cfa76e18abd142a2b923
Jyothi Gadipudi <247127109+jyothigadipudi-tester@users.noreply.github.com>
2026-05-18 14:51:34 +0100

    Adding test cases for calculate sdlt due tax-calculation

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/TaxCalculations/CalculateSDLTDue.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages.TaxCalculations
+
+import uk.gov.hmrc.ui.pages.BasePage
+
+import org.openqa.selenium.By
+
+object CalculateSDLTDue extends BasePage {
+
+  override def pageUrl: String = "tax-calculation/freehold-calculated/calculated-SDLT-due"
+
+  override def pageTitle: String =
+    "Calculated SDLT due - Tax calculation - Freehold calculated - Stamp Taxes Online - GOV.UK"
+
+}
