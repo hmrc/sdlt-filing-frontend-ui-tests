c2e210d70eb93ab66980d017e6986309091f9623
susmitha.chinnari <200940547+susmithachinnari@users.noreply.github.com>
2026-05-22 10:42:06 +0100

    UI tests added for Total premium payable page
    Refactoring

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/Lease/TotalPremiumPayable.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages.Lease
+
+import uk.gov.hmrc.ui.pages.BasePage
+
+object TotalPremiumPayable extends BasePage {
+
+  override def pageUrl: String = "about-the-lease/enter-total-premium-payable"
+
+  override def pageTitle: String =
+    "Calculating the total premium payable including VAT - About the lease - Stamp Taxes Online - GOV.UK"
+
+  val TotalPremiumPayable: String = "value"
+
+  val TotalPremiumPayableInput: String = "2450"
+
+}
