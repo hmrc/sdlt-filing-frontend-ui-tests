bf41b53dfd8b4384d014d142bafe977f0acdb93a
susmitha.chinnari <200940547+susmithachinnari@users.noreply.github.com>
2026-07-07 13:38:53 +0100

    Rebased on latest code

--- src/test/scala/uk/gov/hmrc/ui/specs/DeclarationAndSubmissionSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/DeclarationAndSubmissionSpec.scala
@@ -105,10 +105,10 @@ class DeclarationAndSubmissionSpec
 
       When("the user views their sdlt5 certificate")
       Then("the SubmissionReceipt page is shown")
-      SubmissionComplete.navigateToPage(
+      SubmissionReceipt.navigateToPage(
         "http://localhost:10910/stamp-duty-land-tax-filing/submit-your-return/submission-receipt-and-SDLT5"
       )
-      SubmissionComplete.verifyPageTitle(SubmissionComplete.pageTitleForSubmissionReceipt)
+      SubmissionReceipt.verifyPageTitle(SubmissionReceipt.pageTitle)
     }
   }
 }
