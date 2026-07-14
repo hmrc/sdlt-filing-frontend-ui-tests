48d95332cb2786b3736ad768338c868706979834
susmithachinnari <200940547+susmithachinnari@users.noreply.github.com>
2026-07-14 15:27:13 +0100

    DTR 6909 (#283)
    * UI tests added for completed return page
    
    * Rebased on latest code
    
    * UI tests added for Do you want certificate page

--- src/test/scala/uk/gov/hmrc/ui/specs/DeclarationAndSubmissionSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/DeclarationAndSubmissionSpec.scala
@@ -77,6 +77,12 @@ class DeclarationAndSubmissionSpec
       When("the user enters email address and clicks Save and Continue")
       EnterEmailAddress.input(By.id(EnterEmailAddress.emailAddress), EnterEmailAddress.emailAddressInput)
       EnterEmailAddress.saveAndContinue()
+      Then("the user is navigated to Do you want certificate page")
+      DoYouWantCertificate.verifyPageTitle(DoYouWantCertificate.pageTitle)
+
+      When("the user selects yes and continues")
+      DoYouWantCertificate.radioButton(DoYouWantCertificate.yes)
+      DoYouWantCertificate.saveAndContinue()
       Then("the user is navigated to Who are you submitting this return for")
       WhoAreYouSubmittingThisReturnFor.verifyPageTitle(WhoAreYouSubmittingThisReturnFor.pageTitle)
 
