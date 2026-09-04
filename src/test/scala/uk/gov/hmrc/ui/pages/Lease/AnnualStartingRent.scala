cb6d312cd30b968feceb4d0042b0db075a02ef49
tatheerfatima-lab <236847356+tatheerfatima-lab@users.noreply.github.com>
2026-09-04 16:15:32 +0100

    Content changes on Lease

--- src/test/scala/uk/gov/hmrc/ui/pages/Lease/AnnualStartingRent.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/Lease/AnnualStartingRent.scala
@@ -36,6 +36,6 @@ object AnnualStartingRent extends BasePage {
   def clickDropdownText(): Unit = driver.findElement(By.cssSelector("summary.govuk-details__summary")).click()
 
   def dropdownText: String =
-    "If the exact rent is uncertain (for example, linked to turnover), a reasonable estimate must be made. This should be based on available information, like a business plan or projected turnover, and used to calculate the Net Present Value (NPV) of the rent. Keep evidence of how the estimate was calculated in case requested by HMRC."
+    "If the exact rent is variable or uncertain (for example, linked to turnover), a reasonable estimate must be made. This should be based on available information, like a business plan or projected turnover, and used to calculate the net present value (NPV) of the rent. Keep evidence of how the estimate was calculated in case requested by HMRC."
 
 }
